74fb379d5f2999a644217ee6c948b0c98081f97c
Borut Balazek <bobalazek124@gmail.com>
2023-12-14 14:32:44 +0100

    Reload until API is back alive again

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,43 @@ import { App } from '@moaitime/web-core';
 
 import '@moaitime/web-core/globals.css';
 
+import { API_URL } from '@moaitime/shared-common';
+
+if (import.meta.hot) {
+  // Sometimes the API is not available while it's being rebuild on a change.
+  // We want to try to try to ping the API until it's available.
+  let apiReachableAttempts = 0;
+  const checkIfApiIsAvailable = async () => {
+    if (apiReachableAttempts > 10) {
+      return;
+    }
+
+    try {
+      await fetch(`${API_URL}/health`);
+
+      if (apiReachableAttempts > 0) {
+        window.location.reload();
+      }
+
+      apiReachableAttempts = 0;
+
+      return true;
+    } catch (error) {
+      setTimeout(checkIfApiIsAvailable, 1000);
+
+      return false;
+    } finally {
+      apiReachableAttempts++;
+    }
+  };
+
+  checkIfApiIsAvailable();
+
+  import.meta.hot.on('vite:beforeFullReload', () => {
+    console.log('beforefullrel');
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
--- packages/shared-common/src/Constants.ts
+++ packages/shared-common/src/Constants.ts
@@ -1,13 +1,16 @@
+// Web URL
 export const WEB_URL =
   typeof window !== 'undefined' && window?.location
     ? `${window.location.protocol}//${window.location.hostname}`
     : `http://localhost:4200`;
 
+// API URL
 export const API_URL =
   typeof window !== 'undefined' && window?.location
     ? `${window.location.protocol}//${window.location.hostname}:3636`
     : `http://localhost:3636`;
 
+// WS URL
 const apiUrl = new URL(API_URL);
 const wsPort = apiUrl.port ? `:${apiUrl.port}` : '';
 const wsProtocol = apiUrl.protocol.replace('https', 'wss').replace('http', 'ws') || 'ws';

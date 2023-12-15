d69e010a3179d1b7645c6cd1ac9abeba8eeb3f56
Borut Balazek <bobalazek124@gmail.com>
2023-12-15 07:43:03 +0100

    Remove dark mode check

--- apps/web-e2e/src/e2e/app.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('app.cy.ts', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should have the dark class in the <body> element', () => {
-    cy.get('body').should('have.class', 'dark');
-  });
-});

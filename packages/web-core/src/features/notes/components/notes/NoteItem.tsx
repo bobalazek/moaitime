795862ca801279bea139dbd436e7a8f2dd818838
Borut Balazek <bobalazek124@gmail.com>
2023-12-27 10:18:48 +0100

    More design tweaks on notes

--- packages/web-core/src/features/notes/components/notes/NoteItem.tsx
+++ packages/web-core/src/features/notes/components/notes/NoteItem.tsx
@@ -10,17 +10,20 @@ const NoteItem = ({ note, isSelected }: { note: Note; isSelected?: boolean }) =>
   return (
     <div
       className={clsx(
-        'flex cursor-pointer items-center justify-between px-2 py-2 text-xs hover:bg-gray-50 hover:dark:bg-gray-800',
-        isSelected && 'bg-gray-50 dark:bg-gray-800'
+        'flex cursor-pointer items-center justify-between gap-2 px-2 py-2 text-xs hover:bg-gray-100 hover:dark:bg-gray-700',
+        isSelected && 'bg-gray-200 dark:bg-gray-800'
       )}
       onClick={() => {
         setSelectedNote(note);
       }}
+      data-test="notes--note"
     >
-      <div className="flex items-center">
-        <div className="ml-2 text-sm">{note.title}</div>
+      <div className="truncate text-sm" title={note.title} data-test="notes--note--title">
+        {note.title}
       </div>
-      <div className="text-xs text-gray-400">{new Date(note.createdAt).toLocaleDateString()}</div>
+      <div className="text-[0.65rem] text-gray-400" data-test="notes--note--created-at">
+        {new Date(note.createdAt).toLocaleDateString()}
+      </div>
     </div>
   );
 };
--- packages/web-core/src/features/notes/components/pages/notes/NotesPageSidebar.tsx
+++ packages/web-core/src/features/notes/components/pages/notes/NotesPageSidebar.tsx
@@ -5,7 +5,10 @@ const NotesPageSidebar = () => {
   const { notes, selectedNote } = useNotesStore();
 
   return (
-    <div className="flex h-full w-96 flex-col border-r" data-test="notes--sidebar">
+    <div
+      className="flex h-full w-full max-w-[320px] flex-shrink flex-col border-r"
+      data-test="notes--sidebar"
+    >
       <div data-test="notes--sidebar--inner">
         {notes.length === 0 && (
           <div className="flex h-full items-center justify-center p-4 text-sm text-gray-400">

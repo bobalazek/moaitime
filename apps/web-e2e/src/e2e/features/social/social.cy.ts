b184fa865d9164ac906f8df8e93edede743c2395
anak124 <anakociper124@gmail.com>
2024-02-29 15:36:03 +0100

    Added cypress tests

--- apps/web-e2e/src/e2e/features/social/social.cy.ts
+++ apps/web-e2e/src/e2e/features/social/social.cy.ts
@@ -27,14 +27,56 @@ describe('social.cy.ts', () => {
     cy.getBySel('social--user-search').should('exist');
   });
 
-  it.skip('should go to user profile in social', () => {
+  it('should go to user profile in social', () => {
+    cy.getBySel('social--open-button').click();
+
+    cy.getBySel('social--index').find('[data-test="social--header--user-profile--dropdown-menu--trigger-button"]').click();
+
+    cy.getBySel('social--header--user-profile--dropdown-menu')
+    .find('a[role="menuitem"]')
+    .contains('Profile')
+    .click();
+
+    cy.getBySel('social--users-view--content').should('exist');
+  });
+
+  it('should go to notifications in social', () => {
+    cy.getBySel('social--open-button').click();
+
+    cy.getBySel('social--index').find('[data-test="social--header--user-profile--dropdown-menu--trigger-button"]').click();
+
+    cy.getBySel('social--header--user-profile--dropdown-menu')
+    .find('a[role="menuitem"]')
+    .contains('Notifications')
+    .click();
+
+    cy.getBySel('notifications--content').should('exist');
+  });
+
+  it('should go to settings in social', () => {
     cy.getBySel('social--open-button').click();
 
     cy.getBySel('social--index').find('[data-test="social--header--user-profile--dropdown-menu--trigger-button"]').click();
 
     cy.getBySel('social--header--user-profile--dropdown-menu')
     .find('div[role="menuitem"]')
-    .contains('Profile')
+    .contains('Settings')
     .click();
+
+    cy.getBySel('settings--dialog').should('exist');
   });
+
+  it('should logout the user in social', () => {
+    cy.getBySel('social--open-button').click();
+
+    cy.getBySel('social--index').find('[data-test="social--header--user-profile--dropdown-menu--trigger-button"]').click();
+
+    cy.getBySel('social--header--user-profile--dropdown-menu')
+    .find('div[role="menuitem"]')
+    .contains('Logout')
+    .click();
+
+    cy.get('div').contains('Sign In').should('exist');
+  });
+
 });

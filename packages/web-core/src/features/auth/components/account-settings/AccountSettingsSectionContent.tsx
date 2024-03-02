ec47a6ba3dc5f7a0690153d02eef93c5cc870fc2
anak124 <anakociper124@gmail.com>
2024-03-02 15:00:15 +0100

    Prettier fix

--- packages/web-core/src/features/auth/components/account-settings/AccountSettingsSectionContent.tsx
+++ packages/web-core/src/features/auth/components/account-settings/AccountSettingsSectionContent.tsx
@@ -191,7 +191,7 @@ export default function AccountSettingsSectionContent() {
         <h4 className="text-lg font-bold">Display Name</h4>
         <p className="mb-2 text-xs text-gray-400">What shall we call you?</p>
         <Input
-          id='account-settings-display-name'
+          id="account-settings-display-name"
           value={userDisplayName}
           onChange={(event) => {
             setUserDisplayName(event.target.value);
@@ -202,7 +202,7 @@ export default function AccountSettingsSectionContent() {
         <h4 className="text-lg font-bold">Username</h4>
         <p className="mb-2 text-xs text-gray-400">What about your username?</p>
         <Input
-        id='account-settings-username'
+          id="account-settings-username"
           value={userUsername}
           onChange={(event) => {
             setUserUsername(event.target.value);
@@ -213,7 +213,7 @@ export default function AccountSettingsSectionContent() {
         <h4 className="text-lg font-bold">Email</h4>
         <p className="mb-2 text-xs text-gray-400">What is your email?</p>
         <Input
-          id='account-settings-email'
+          id="account-settings-email"
           value={userEmail}
           onChange={(event) => {
             setUserEmail(event.target.value);
@@ -277,7 +277,7 @@ export default function AccountSettingsSectionContent() {
           Anything interesting you want to tell us about yourself?
         </p>
         <Textarea
-          id='account-settings-bio'
+          id="account-settings-bio"
           value={userBiography}
           onChange={(event) => {
             setUserBiography(event.target.value);

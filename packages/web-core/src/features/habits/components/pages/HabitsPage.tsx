93be5fff41d8603014530d08a111d233f1237e43
Borut Balazek <bobalazek124@gmail.com>
2024-03-04 13:06:17 +0100

    State stuff

--- packages/web-core/src/features/habits/components/pages/HabitsPage.tsx
+++ packages/web-core/src/features/habits/components/pages/HabitsPage.tsx
@@ -1,16 +1,55 @@
-import { useEffect } from 'react';
+import { format } from 'date-fns';
+import { useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { ErrorBoundary } from '../../../core/components/ErrorBoundary';
 import { useEscapeToHome } from '../../../core/hooks/useEscapeToHome';
+import { useStateAndUrlSync } from '../../../core/hooks/useStateAndUrlSync';
+import { useHabitsStore } from '../../state/habitsStore';
 import HabisPageHeader from './habits/HabitsPageHeader';
 import HabitsPageMain from './habits/HabitsPageMain';
 
 export default function HabitsPage() {
+  const { selectedDate, setSelectedDate } = useHabitsStore();
+  const location = useLocation();
+  const [targetUri, setTargetUri] = useState(`${location.pathname}${location.search}`);
+  const isInitializedRef = useRef(false);
+
   useEscapeToHome();
 
   useEffect(() => {
     document.title = 'Habits | MoaiTime';
   }, []);
+
+  const updateStateByUrl = useDebouncedCallback(() => {
+    const params = new URLSearchParams(location.search);
+    const newSelectedDate = params.get('selectedDate');
+    if (newSelectedDate && newSelectedDate !== format(selectedDate, 'yyyy-MM-dd')) {
+      setSelectedDate(new Date(newSelectedDate));
+    }
+  }, 10);
+
+  useEffect(() => {
+    if (isInitializedRef.current) {
+      return;
+    }
+
+    isInitializedRef.current = true;
+
+    updateStateByUrl();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const selectedDateString = format(selectedDate, 'yyyy-MM-dd');
+    const newTargetUri = `/habits?selectedDate=${selectedDateString}`;
+
+    setTargetUri(newTargetUri);
+  }, [setTargetUri, selectedDate]);
+
+  useStateAndUrlSync(updateStateByUrl, targetUri);
 
   return (
     <ErrorBoundary>

f87e9cf957148a264e69650a80eeab704827fa2b
Borut Balazek <bobalazek124@gmail.com>
2024-03-04 07:19:45 +0100

    Back icon in header instead of home icon

--- packages/web-core/src/features/core/components/layout/LayoutPageHeader.tsx
+++ packages/web-core/src/features/core/components/layout/LayoutPageHeader.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon } from 'lucide-react';
+import { ArrowLeftIcon } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 export type LayoutPageHeaderProps = {
@@ -15,15 +15,15 @@ const LayoutPageHeader = ({ testKey, title, children }: LayoutPageHeaderProps) =
       className="flex items-center justify-between gap-4 border-b px-4 py-3 text-center text-2xl"
       data-test={`${testKey}--header`}
     >
-      <div className="flex space-x-2 align-middle">
+      <div className="flex gap-4 align-middle">
         <button
           onClick={() => {
             navigate('/');
           }}
-          title="Go home"
+          title="Go Back Home"
           data-test={`${testKey}--header--home-button`}
         >
-          <HomeIcon />
+          <ArrowLeftIcon />
         </button>
         <div>{title}</div>
       </div>

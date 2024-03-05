dc56511ef79c3c0a07c1784026f260bf6ecbd188
Borut Balazek <bobalazek124@gmail.com>
2024-03-05 12:44:18 +0100

    Fix when no habits are present

--- packages/web-core/src/features/habits/components/habits-daily-list/HabitsDailyList.tsx
+++ packages/web-core/src/features/habits/components/habits-daily-list/HabitsDailyList.tsx
@@ -20,10 +20,28 @@ const HabitsDailyList = () => {
 
   return (
     <div>
-      <div data-test="habits-daily-list" className="flex flex-col gap-4">
-        {data.map((habitDaily) => (
-          <HabitDailyEntry key={habitDaily.id} habitDaily={habitDaily} />
-        ))}
+      <div data-test="habits-daily-list">
+        {data.length === 0 && (
+          <div className="text-muted-foreground justify-center text-center">
+            <div className="mb-3 text-3xl">No habits just yet.</div>
+            <div>
+              Would be nice to add at least one, so this page wouldn't be so empty, wouldn't it?
+            </div>
+            <div className="absolute right-4 top-14 select-none text-center text-5xl">
+              <div>☝️</div>
+              <div className="mt-2 text-[0.65rem] leading-tight">
+                Press here. <br /> Gently please.
+              </div>
+            </div>
+          </div>
+        )}
+        {data.length > 0 && (
+          <div className="flex flex-col gap-4">
+            {data.map((habitDaily) => (
+              <HabitDailyEntry key={habitDaily.id} habitDaily={habitDaily} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );

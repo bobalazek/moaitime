c18d96d58acf7e4f97cf4bbedfcfd01eab8d647a
Borut Balazek <bobalazek124@gmail.com>
2024-03-06 10:06:59 +0100

    Disable dts building in tsup when it's NODE_ENV === 'test'

--- apps/api/src/websocket/controllers/websocket.controller.ts
+++ apps/api/src/websocket/controllers/websocket.controller.ts
@@ -20,6 +20,7 @@ export class WebsocketController {
     return {
       data: {
         websocketUrl,
+        a: 'te',
       },
     };
   }
--- apps/cli/tsup.config.ts
+++ apps/cli/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/main.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: process.env.NODE_ENV !== 'test',
   sourcemap: true,
   clean: true,
   shims: true,

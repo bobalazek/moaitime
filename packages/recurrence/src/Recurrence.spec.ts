0dc87fd802a0ab8c47e1b80a9b8b4b0b1a1ce11b
Borut Balazek <bobalazek124@gmail.com>
2024-03-15 18:51:53 +0100

    More test tweaks

--- packages/recurrence/src/Recurrence.spec.ts
+++ packages/recurrence/src/Recurrence.spec.ts
@@ -15,7 +15,7 @@ describe('Recurrence.ts', () => {
   it.each([
     // Hourly
     {
-      testName: 'should work when hourly interval #1',
+      testName: 'should work hourly',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -26,10 +26,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-01T01:00:00.000',
         humanText: 'every hour starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when hourly interval #2',
+      testName: 'should work every 4 hours',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -40,10 +39,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-01T04:00:00.000',
         humanText: 'every 4 hours starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when hourly interval #3',
+      testName: 'should work hourly but only at 2:00 and 3:00',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -55,10 +53,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-01T02:00:00.000',
         humanText: 'every hour at 2:00, 3:00 starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when hourly interval #4',
+      testName: 'should work hourly but only at 2:00 and 3:00 on a Saturday',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -71,10 +68,10 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-04T02:00:00.000',
         humanText: 'every hour on Saturday at 2:00, 3:00 starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when hourly interval #5',
+      testName:
+        'should work hourly but only at 2:00 and 3:00 on a Wednesday and the 15th of the month',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -89,11 +86,10 @@ describe('Recurrence.ts', () => {
         humanText:
           'every hour on Wednesday at 2:00, 3:00 on the 15th of the month starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     // Daily
     {
-      testName: 'should work when daily interval #1',
+      testName: 'should work daily',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -104,10 +100,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-02T00:00:00.000',
         humanText: 'every day starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when daily interval #2',
+      testName: 'should work every 3 days',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -118,10 +113,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-04T00:00:00.000',
         humanText: 'every 3 days starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when daily interval #3',
+      testName: 'should work daily but only 5 iterations',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -140,7 +134,8 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #4',
+      testName:
+        'should work when daily with 10 iterations and an end date earlier than the 10th iteration',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -162,7 +157,8 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #5',
+      testName:
+        'should work when daily with 10 iterations starting after now and an end date only 2 iterations after the start date',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-06T00:00:00.000'),
@@ -176,20 +172,7 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #6',
-      now: '2020-01-01T00:00:00.000',
-      options: {
-        startsAt: new Date('2020-01-06T00:00:00.000'),
-        interval: RecurrenceIntervalEnum.DAY,
-        intervalAmount: 1,
-        count: 10,
-      },
-      expected: {
-        nextDate: '2020-01-07T00:00:00.000',
-      },
-    },
-    {
-      testName: 'should work when daily interval #7',
+      testName: 'should work daily with only the next date',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-06T00:00:00.000'),
@@ -201,7 +184,7 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #8',
+      testName: 'should work daily with dates between',
       now: '2020-01-01T00:00:00.000',
       startDate: '2020-01-02T00:00:00.000', // Those 2 dates are only needed if we have dates between
       endDate: '2020-01-12T00:00:00.000',
@@ -220,7 +203,7 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #9',
+      testName: 'should work daily with dates between and an end date option',
       now: '2020-01-01T00:00:00.000',
       startDate: '2020-01-02T00:00:00.000',
       endDate: '2020-01-12T00:00:00.000',
@@ -235,7 +218,7 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #10',
+      testName: 'should work daily with dates between and an end date option and 4 iterations',
       now: '2020-01-01T00:00:00.000',
       startDate: '2020-01-02T00:00:00.000',
       endDate: '2020-01-12T00:00:00.000',
@@ -251,7 +234,7 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #11',
+      testName: 'should work daily with dates between and an end date option and 1 iteration',
       now: '2020-01-01T00:00:00.000',
       startDate: '2020-01-02T00:00:00.000',
       endDate: '2020-01-12T00:00:00.000',
@@ -267,9 +250,9 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #12',
+      testName: 'should work daily with dates between and an count option',
       now: '2020-01-01T00:00:00.000',
-      startDate: '2020-01-02T00:00:00.000', // Those 2 dates are only needed if we have dates between
+      startDate: '2020-01-02T00:00:00.000',
       endDate: '2020-01-12T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-08T00:00:00.000'),
@@ -286,20 +269,19 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #13',
+      testName: 'should work daily with the next date option',
       now: '2020-01-14T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-14T00:00:00.000'),
         interval: RecurrenceIntervalEnum.DAY,
         intervalAmount: 1,
-        count: 3,
       },
       expected: {
         nextDate: '2020-01-15T00:00:00.000',
       },
     },
     {
-      testName: 'should work when daily interval #14',
+      testName: 'should work daily with the next date option and an end date option',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-06T00:00:00.000'),
@@ -312,7 +294,7 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #15',
+      testName: 'should work daily with next dates and an end date option with 10 iterations',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-06T00:00:00.000'),
@@ -326,7 +308,8 @@ describe('Recurrence.ts', () => {
       },
     },
     {
-      testName: 'should work when daily interval #16',
+      testName:
+        'should work daily with 3 iterations and an end date option and only on a Wednesday',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-06T00:00:00.000'),
@@ -345,7 +328,7 @@ describe('Recurrence.ts', () => {
     },
     // Weekly
     {
-      testName: 'should work when weekly interval #1',
+      testName: 'should work weekly',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -356,10 +339,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-08T00:00:00.000',
         humanText: 'every week starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when weekly interval #2',
+      testName: 'should work every 2 weeks',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -370,10 +352,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-01-15T00:00:00.000',
         humanText: 'every 2 weeks starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when weekly interval #3',
+      testName: 'should work weekly but only on a Wednesday',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-06T00:00:00.000'),
@@ -392,7 +373,7 @@ describe('Recurrence.ts', () => {
     },
     // Monthly
     {
-      testName: 'should work when monthly interval #1',
+      testName: 'should work monthly',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -403,10 +384,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-02-01T00:00:00.000',
         humanText: 'every month starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when monthly interval #2',
+      testName: 'should work every 2 months',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -417,11 +397,10 @@ describe('Recurrence.ts', () => {
         nextDate: '2020-03-01T00:00:00.000',
         humanText: 'every 2 months starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     // Yearly
     {
-      testName: 'should work when yearly interval #1',
+      testName: 'should work yearly',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -432,10 +411,9 @@ describe('Recurrence.ts', () => {
         nextDate: '2021-01-01T00:00:00.000',
         humanText: 'every year starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     {
-      testName: 'should work when yearly interval #2',
+      testName: 'should work every 2 years',
       now: '2020-01-01T00:00:00.000',
       options: {
         startsAt: new Date('2020-01-01T00:00:00.000'),
@@ -446,7 +424,6 @@ describe('Recurrence.ts', () => {
         nextDate: '2022-01-01T00:00:00.000',
         humanText: 'every 2 years starting 1/1/2020',
       },
-      throwsErrorMessage: undefined,
     },
     // Errors
     {

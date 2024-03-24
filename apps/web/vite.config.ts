978ca78edf9bcd4ed4c5f99ca7f28921e63afba5
Borut Balazek <bobalazek124@gmail.com>
2024-03-24 19:01:39 +0100

    Build fixes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,30 @@
+import { join } from 'path';
+
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  server: {
-    port: 4200,
-    host: '0.0.0.0',
-    hmr: true,
-    fs: {
-      // https://github.com/vitejs/vite/issues/15784
-      cachedChecks: false,
+export default defineConfig((options) => {
+  const envDir = join(__dirname, '..', '..');
+  const envPrefix = ['VITE_', 'OAUTH_GOOGLE_CLIENT_ID', 'WEB_BASE_URL', 'API_BASE_URL'];
+
+  return {
+    ...options,
+    server: {
+      port: 4200,
+      host: '0.0.0.0',
+      hmr: true,
+      fs: {
+        // https://github.com/vitejs/vite/issues/15784
+        cachedChecks: false,
+      },
     },
-  },
-  preview: {
-    port: 4300,
-    host: '0.0.0.0',
-  },
-  plugins: [react(), tsconfigPaths()],
+    preview: {
+      port: 4300,
+      host: '0.0.0.0',
+    },
+    plugins: [react(), tsconfigPaths()],
+    envDir,
+    envPrefix,
+  };
 });
--- packages/shared-frontend/tsup.config.ts
+++ packages/shared-frontend/tsup.config.ts
@@ -1,35 +1,10 @@
 import { defineConfig } from 'tsup';
 
-const whitelistPrefixes = ['OAUTH_GOOGLE_CLIENT_ID'];
-
-export default defineConfig(async (options) => {
-  // A very fucking stupid workaround, as for some reason I can't import the shared-backend library the normal way ...
-  const { loadEnvironmentVariables } = await import(
-    __dirname + '/node_modules/@moaitime/shared-backend/dist/index.js'
-  );
-
-  loadEnvironmentVariables();
-
-  const customEnv: Record<string, string> = {};
-  for (const [key, value] of Object.entries(process.env)) {
-    if (whitelistPrefixes.some((prefix) => key.startsWith(prefix))) {
-      customEnv[key] = value as string;
-    }
-  }
-
-  const env = {
-    ...options.env,
-    ...customEnv,
-  };
-
-  return {
-    ...options,
-    env,
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    dts: process.env.GENERATE_DTS === 'true',
-    sourcemap: true,
-    clean: true,
-    shims: true,
-  };
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: process.env.GENERATE_DTS === 'true',
+  sourcemap: true,
+  clean: true,
+  shims: true,
 });

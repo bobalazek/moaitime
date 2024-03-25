a48e2cae4adc8cc9528ab9165c2f266bd7a4662c
Borut Balazek <bobalazek124@gmail.com>
2024-03-25 15:10:45 +0100

    Fix sonner toast outside interaction closing the popover

--- packages/web-ui/src/components/popover.tsx
+++ packages/web-ui/src/components/popover.tsx
@@ -10,7 +10,7 @@ const PopoverTrigger = PopoverPrimitive.Trigger;
 const PopoverContent = forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+>(({ className, align = 'center', sideOffset = 4, onInteractOutside, ...props }, ref) => (
   <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
       ref={ref}
@@ -20,6 +20,16 @@ const PopoverContent = forwardRef<
         'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border p-4 shadow-md outline-none',
         className
       )}
+      onInteractOutside={(event) => {
+        // Check the sonner-toaster.tsx component for more information.
+        // This is a hacky workaround to prevent closing of dialog,
+        // if you click somewhere inside the toaster.
+        if (event.target instanceof HTMLElement && event.target.closest('[data-sonner-toast]')) {
+          event.preventDefault();
+        }
+
+        onInteractOutside?.(event);
+      }}
       {...props}
     />
   </PopoverPrimitive.Portal>

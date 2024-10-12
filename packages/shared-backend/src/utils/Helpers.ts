c237e316ca48bc971b1d6b095912c8b7deb76da6
Borut Balazek <bobalazek124@gmail.com>
2024-10-12 18:32:35 +0200

    chore: change the way we handle environment variables

--- packages/shared-backend/src/utils/Helpers.ts
+++ packages/shared-backend/src/utils/Helpers.ts
@@ -8,16 +8,20 @@ import { ROOT_DIR } from '../Constants';
 export const loadEnvironmentVariables = () => {
   const NODE_ENV = process.env.NODE_ENV || 'development';
   const files = [`.env.${NODE_ENV}.local`, `.env.local`, `.env.${NODE_ENV}`, `.env`];
+
+  const path: string[] = [];
   files.forEach((file) => {
     const envFilePath = resolve(join(ROOT_DIR, file));
     if (!existsSync(envFilePath)) {
       return;
     }
 
-    console.log(`Loading environment variables from ${envFilePath} ...`);
-
-    dotenv.config({ path: envFilePath });
+    path.push(envFilePath);
   });
+
+  console.log(`Loading environment variables from "${path.join(', ')}" ...`);
+
+  dotenv.config({ path: path });
 
   return {
     NODE_ENV,

d96eef0b7b31e2035769ca243dc90b20ddd58833
Borut Balazek <bobalazek124@gmail.com>
2025-03-08 14:12:07 +0100

    chore: revert to original environment values

--- packages/shared-frontend/src/Constants.ts
+++ packages/shared-frontend/src/Constants.ts
@@ -5,16 +5,14 @@ declare global {
   }
 }
 
-const isWindowDefined = typeof window !== 'undefined';
-
 // API URL
 export const API_URL =
-  (isWindowDefined && window?.globals?.['API_URL']) ||
+  (typeof window !== 'undefined' && window?.globals?.['API_URL']) ||
   (typeof import.meta !== 'undefined' && import.meta.env?.API_URL) ||
   `http://localhost:3636`;
 
 // OAuth
 export const OAUTH_GOOGLE_CLIENT_ID =
-  (isWindowDefined && window?.globals?.['OAUTH_GOOGLE_CLIENT_ID']) ||
+  (typeof window !== 'undefined' && window?.globals?.['OAUTH_GOOGLE_CLIENT_ID']) ||
   (typeof import.meta !== 'undefined' && import.meta.env?.OAUTH_GOOGLE_CLIENT_ID) ||
   '';

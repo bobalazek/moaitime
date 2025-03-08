5ce7a9968d4e740e2cdba43d6733cc33cf045310
Borut Balazek <bobalazek124@gmail.com>
2025-03-08 13:35:19 +0100

    chore: clean redundant vite variables

--- packages/shared-frontend/src/Constants.ts
+++ packages/shared-frontend/src/Constants.ts
@@ -7,7 +7,6 @@ declare global {
 
 // API URL
 export const API_URL =
-  import.meta.env?.API_URL ||
   (typeof window !== 'undefined' && window?.globals ? window?.globals?.['API_URL'] : null) ||
   (typeof window !== 'undefined' && window?.location
     ? `${window.location.protocol}//${window.location.hostname}:3636`
@@ -15,8 +14,6 @@ export const API_URL =
 
 // OAuth
 export const OAUTH_GOOGLE_CLIENT_ID =
-  import.meta.env?.OAUTH_GOOGLE_CLIENT_ID ||
   (typeof window !== 'undefined' && window?.globals
     ? window?.globals?.['OAUTH_GOOGLE_CLIENT_ID']
-    : null) ||
-  '';
+    : null) || '';

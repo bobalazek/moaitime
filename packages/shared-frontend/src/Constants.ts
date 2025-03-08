e81820bf8dcd6c34f69074c6f186c72f7cccf227
Borut Balazek <bobalazek124@gmail.com>
2025-03-08 14:10:56 +0100

    chore(web): another env tweak

--- packages/shared-frontend/src/Constants.ts
+++ packages/shared-frontend/src/Constants.ts
@@ -5,10 +5,16 @@ declare global {
   }
 }
 
+const isWindowDefined = typeof window !== 'undefined';
+
 // API URL
 export const API_URL =
-  window?.globals?.['API_URL'] || import.meta.env?.API_URL || `http://localhost:3636`;
+  (isWindowDefined && window?.globals?.['API_URL']) ||
+  (typeof import.meta !== 'undefined' && import.meta.env?.API_URL) ||
+  `http://localhost:3636`;
 
 // OAuth
 export const OAUTH_GOOGLE_CLIENT_ID =
-  window?.globals?.['OAUTH_GOOGLE_CLIENT_ID'] || import.meta.env?.OAUTH_GOOGLE_CLIENT_ID || '';
+  (isWindowDefined && window?.globals?.['OAUTH_GOOGLE_CLIENT_ID']) ||
+  (typeof import.meta !== 'undefined' && import.meta.env?.OAUTH_GOOGLE_CLIENT_ID) ||
+  '';
